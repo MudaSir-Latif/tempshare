c0e54e38949cfbd111c63918682a3701b710d374
Ubuntu <ubuntu@ip-172-31-33-89.eu-north-1.compute.internal>
2025-10-12 02:06:18 +0000

    Fix lint error

--- temshare_proj/frontend/views.py
+++ temshare_proj/frontend/views.py
@@ -2,7 +2,7 @@
 
 # Create your views here.
 from django.shortcuts import render
-from core.views import upload_file,submit_url,serve_file,redirect_url
+from core.views import upload_file, submit_url, serve_file, redirect_url
 
 # def home(request):
 #     return render(request, "frontend/home.html")
